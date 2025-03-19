42f65e902930e1892b2c22df01b136a43fc3096e
Jesse Holzman <jesseholzman96@gmail.com>
2025-03-19 17:08:32 -0400

    Basic TCP client/server communication

--- src/prover/main.rs
+++ src/prover/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(stream: &mut TcpStream) {
+    stream.write(b"AYOOOOOOOOOOOOOOOOOOOOOOOO").unwrap();
+
+    let mut buf = [0; 256];
+    stream.read(&mut buf).unwrap();
+
+    println!("Client says: {}", String::from_utf8_lossy(&buf));
+}
+
 fn main() {
     println!("I am the prover");
+    let listener = TcpListener::bind("127.0.0.1:9876").unwrap();
+
+    for stream in listener.incoming() {
+        println!("New client connected.");
+        handle_client(&mut stream.unwrap());
+    }
 }
--- src/verifier/main.rs
+++ src/verifier/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
 fn main() {
     println!("I am the verifier");
+
+    let mut stream = TcpStream::connect("127.0.0.1:9876").unwrap();
+
+    stream
+        .write(b"hi its me peter griffin from family guy.")
+        .unwrap();
+
+    let mut buf = [0; 256];
+    stream.read(&mut buf).unwrap();
+
+    println!("Server says: {}", String::from_utf8_lossy(&buf));
 }
