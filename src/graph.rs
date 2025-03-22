ece8eb389bbf370ed9af24ce2bbc92ff3ae09d76
Jesse Holzman <jesseholzman96@gmail.com>
2025-03-22 18:00:22 -0400

    Add methods to graph for adding vertices and edges

--- src/graph.rs
+++ src/graph.rs
@@ -15,6 +15,33 @@ impl Graph {
         }
     }
 
+    pub fn add_vertex(&mut self, v: VertexId) {
+        if let Some(_) = self.edges.insert(v, vec![]) {
+            println!("Warning: overwrote vertex in graph with ID {}", v);
+        }
+    }
+
+    pub fn add_edge(&mut self, u: VertexId, v: VertexId) {
+        if !self.contains_edge(u, v) {
+            if !self.edges.contains_key(&u) {
+                self.edges.insert(u, vec![]);
+            }
+
+            self.edges.get_mut(&u).unwrap().push(v);
+        }
+    }
+
+    pub fn contains_edge(&self, u: VertexId, v: VertexId) -> bool {
+        // graph is undirected so check for either u->v or v->u edge
+        if let Some(vs) = self.edges.get(&u) {
+            vs.contains(&v)
+        } else if let Some(us) = self.edges.get(&v) {
+            us.contains(&u)
+        } else {
+            false
+        }
+    }
+
     pub fn vertices(&self) -> impl Iterator<Item = VertexId> + '_ {
         self.edges.keys().copied()
     }
