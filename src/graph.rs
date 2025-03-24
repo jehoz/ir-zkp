caeab5c23d9f3c9e96152091d94b7570f36a7cd7
Jesse Holzman <jesseholzman96@gmail.com>
2025-03-24 09:48:38 -0400

    Add docstrings to graph methods

--- src/graph.rs
+++ src/graph.rs
@@ -2,12 +2,15 @@ use std::collections::HashMap;
 
 pub type VertexId = i32;
 
+/// A simple undirected, unweighted graph.
+/// Vertices can be labelled, letting us "color" the graph.
 pub struct Graph {
     edges: HashMap<VertexId, Vec<VertexId>>,
     labels: HashMap<VertexId, String>,
 }
 
 impl Graph {
+    /// Creates a new empty graph.
     pub fn new() -> Graph {
         Graph {
             edges: HashMap::new(),
@@ -15,34 +18,43 @@ impl Graph {
         }
     }
 
+    /// Inserts a vertex with specified vertex ID.  
+    /// If a vertex with that ID already exists in the graph, this function does nothing.
     pub fn add_vertex(&mut self, v: VertexId) {
-        if let Some(_) = self.edges.insert(v, vec![]) {
-            println!("Warning: overwrote vertex in graph with ID {}", v);
+        if !self.contains_vertex(v) {
+            self.edges.insert(v, vec![]);
         }
     }
 
+    /// Creates an edge between vertices u and v in the graph.  
+    /// If either vertex does not yet exist in the graph, it will be added.
+    /// If the edge already exists, this function does nothing.
     pub fn add_edge(&mut self, u: VertexId, v: VertexId) {
         if !self.contains_edge(u, v) {
-            if !self.contains_vertex(u) {
-                self.add_vertex(u);
-            }
-
+            self.add_vertex(u);
+            self.add_vertex(v);
             self.edges.get_mut(&u).unwrap().push(v);
         }
     }
 
+    /// Returns the label for a specified vertex (if one exists).
     pub fn get_label(&self, u: VertexId) -> Option<String> {
         self.labels.get(&u).cloned()
     }
 
+    /// Labels a vertex in the graph.
+    /// If the vertex already has a label, it will be overwritten.
     pub fn set_label(&mut self, u: VertexId, label: String) {
         self.labels.insert(u, label);
     }
 
+    /// Returns whether or not the graph contains a vertex with the specified ID.
     pub fn contains_vertex(&self, v: VertexId) -> bool {
         self.edges.contains_key(&v)
     }
 
+    /// Returns whether or not the graph contains an edge between the specified vertices.
+    /// Because the graph is undirected, the order of vertices given doesn't matter.
     pub fn contains_edge(&self, u: VertexId, v: VertexId) -> bool {
         // graph is undirected so check for either u->v or v->u edge
         if let Some(vs) = self.edges.get(&u) {
@@ -54,10 +66,12 @@ impl Graph {
         }
     }
 
+    /// Returns an iterator traversing all of the vertices in the graph.
     pub fn vertices(&self) -> impl Iterator<Item = VertexId> + '_ {
         self.edges.keys().copied()
     }
 
+    /// Returns an iterator traversing all of the unique edges in the graph.
     pub fn edges(&self) -> impl Iterator<Item = (VertexId, VertexId)> + '_ {
         self.edges
             .iter()
