cf9232629efadfc5771a1614eaa0204e2487bcb3
Jesse Holzman <jesseholzman96@gmail.com>
2025-03-30 10:04:44 -0400

    Parse graph on verifier side

--- src/bin/verifier.rs
+++ src/bin/verifier.rs
@@ -1,6 +1,9 @@
 use std::net::TcpStream;
 
-use ir_zkp::message::{read_message, write_message, ProverMessage, VerifierMessage};
+use ir_zkp::{
+    graph::Graph,
+    message::{read_message, write_message, ProverMessage, VerifierMessage},
+};
 
 fn main() {
     println!("I am the verifier");
@@ -11,5 +14,27 @@ fn main() {
 
     let response: ProverMessage = read_message(&mut stream).unwrap();
 
-    println!("Server says: {:?}", response);
+    match response {
+        ProverMessage::GraphInfo {
+            adjacency_list,
+            colors,
+        } => {
+            let g = Graph::from_adjacency_list(adjacency_list);
+
+            let num_edges = g.edges().count();
+            println!("Received graph with {} edges.", num_edges);
+            let rounds = {
+                let prob_invalid = (num_edges as f32 - 1.0) / (num_edges as f32);
+                let negligible = 0.5f32.powi(128);
+                (negligible.log(prob_invalid)).ceil() as i32
+            };
+            println!(
+                "Need to perform {} rounds of verification before accepting proof.",
+                rounds
+            );
+        }
+        _ => {
+            println!("Unexpected response type: {:?}", response)
+        }
+    }
 }
